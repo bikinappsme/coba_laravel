cdfe342bb0cee4a9cb11857944c79885f52f0c5d
rinehaxor <rinehaxor@users.noreply.github.com>
2025-05-09 19:45:28 +0700

    add:routing web

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Contact')  
+
+@section('content')
+
+    <h1>Kontak Kami</h1>
+    <p>Email: Bikinappsme@gmail.com</p>
+    <p>Tiktok: @bikinapps</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home')  
+
+@section('content')
+
+    <h1>Selamat Datang di Halaman Home Bikin Apps!</h1>
+
+@endsection
